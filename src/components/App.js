d0214e0e6926865e687216904e13c2ecd55406f7
Safx <safxdev@gmail.com>
2017-05-31 01:09:50 +0900

    add Slate component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import styles from './App.css'
+import { Editor, Raw } from 'slate'
 
-const App = () => (
-    <div className={styles.app}>
-        <h2>Hello</h2>
-    </div>
-)
+const initialState = Raw.deserialize({
+  nodes: [
+    {
+      kind: 'block',
+      type: 'paragraph',
+      nodes: [
+        {
+          kind: 'text',
+          text: 'A line of text in a paragraph.'
+        }
+      ]
+    }
+  ]
+}, { terse: true })
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            state: initialState
+        }
+    }
+
+    onChange(state) {
+        console.log(state)
+        this.setState({ state })
+    }
+
+    render() {
+        return (<Editor
+                state={this.state.state}
+                onChange={this.onChange.bind(this)}
+                />
+               )
+    }
+}
 
 export default App
